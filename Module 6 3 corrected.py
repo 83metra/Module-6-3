c9d44800324a2eca677538cb782b1686532a239a
83metra <jumo_004@mail.ru>
2024-07-26 00:35:59 +0200

    Update Module 6 3 corrected.py
    "Исправлено:
    - fly и run меняют внутреннее состояние, они не должны ничего возвращать"  ----- Убрано return
    
    
    "- зачем в move
    self.dx = dx
    self.dy = dy ?"   ---- теперь dx и dy напрямую передаются в run(dx) и fly(dy).
    
    
    "- __sound = 'Frrr' - зачем 2 нижних подчеркивания? используйте такое же название, как в орле - просто в результате наследовния вы перезапишите 1 переменную другой (лошадь орлом)"  ------- подчёркивания убраны, теперь перезаписано voice(self): return print(Eagle.sound)

--- Module 6 3 corrected.py
+++ Module 6 3 corrected.py
@@ -1,32 +1,29 @@
 class Horse:
-    x_distance = 0        # пройденный путь
-    __sound = 'Frrr'  
+    x_distance = 0  # пройденный путь
+    sound = 'Frrr'  # звук, который издаёт лошадь.
     
-    def run(self):
-        self.x_distance += self.dx
-        return self.x_distance
+    def run(self, dx):
+        self.x_distance += dx
 
 class Eagle:
-    y_distance = 0                             # высота полёта
-    sound = 'I train, eat, sleep, and repeat!' # звук, который издаёт орёл (отсылка)
+    y_distance = 0  # высота полёта
+    sound = 'I train, eat, sleep, and repeat!'  # звук, который издаёт орёл (отсылка)
+    
+    def fly(self, dy):
+        self.y_distance += dy
 
-    def fly(self):
-        self.y_distance += self.dy
-        return self.y_distance
 
 class Pegasus(Horse, Eagle):
+    def move(self, dx, dy):  # где dx и dy изменения дистанции.
+                             # В этом методе должны запускаться наследованные методы run и fly соответственно.
+        self.run(dx)
+        self.fly(dy)
 
-    def move(self, dx, dy): # где dx и dy изменения дистанции.
-                            # В этом методе должны запускаться наследованные методы run и fly соответственно.
-        self.dx = dx
-        self.dy = dy
-        self.run()
-        self.fly()
-        return
-    def get_pos(self): # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке.
+    def get_pos(self):  # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке.
         return (self.x_distance, self.y_distance)
+
     def voice(self):
-        return print(self.sound)
+        return print(Eagle.sound)
 
 
 p1 = Pegasus()
